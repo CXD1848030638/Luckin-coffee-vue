4f2e1806de30e785aa17af001168c4557044023b
CXD1848030638 <cxd1848030638@gmail.com>
2024-06-30 20:57:53 +0800

    细节完善

--- src/views/Mine/AddressView.vue
+++ src/views/Mine/AddressView.vue
@@ -26,21 +26,6 @@ const router = useRouter()
 
 const chosenAddressId = ref('1');
 const list = ref([])
-// const list = [
-//     {
-//         id: '1',
-//         name: '张三',
-//         tel: '13000000000',
-//         address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
-//         isDefault: true,
-//     },
-//     {
-//         id: '2',
-//         name: '李四',
-//         tel: '1310000000',
-//         address: '浙江省杭州市拱墅区莫干山路 50 号',
-//     }
-// ];
 
 //查询已保存的所有地址
 const addressList = ref([])
